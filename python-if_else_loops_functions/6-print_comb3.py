a6a6cc3b5ffa7e9e440b232e2305b4c917de2b5f
batrousseau <batrousseau@hotmail.com>
2026-05-18 20:55:45 +0200

    Deleting final space to match required lenght

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(0, 9):
     for j in range(1, 10):
         if i == 8 and j == 9:
-            print("{}{} ".format(i, j))
+            print("{}{}".format(i, j))
         elif j > i:
             print("{}{},".format(i, j), end=' ')
