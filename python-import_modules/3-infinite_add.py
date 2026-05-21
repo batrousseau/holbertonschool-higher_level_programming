470a73813fc260dc00bfcb39315b703259bbb34a
batrousseau <batrousseau@hotmail.com>
2026-05-21 16:04:32 +0200

    Initialization of task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+__name__ = "__main__"
+
+import sys
+
+result: int = 0
+
+
+for i in range (1, len(sys.argv)):
+	result = result + int(sys.argv[i])
+
+print("{}".format(result))
