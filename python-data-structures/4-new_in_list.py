df0616a5ef2d9084aa459c6951e0682319ae37a6
batrousseau <batrousseau@hotmail.com>
2026-05-26 15:41:40 +0200

    Initialization of task 4

---
+++ python-data-structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
+        copy_list = my_list.copy()
+        del copy_list[idx]
+        copy_list.insert(idx, element)
+        return (copy_list)
