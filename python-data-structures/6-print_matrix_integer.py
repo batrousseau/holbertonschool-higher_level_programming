f20a3d01d965d60b5149134332861ee59975e626
batrousseau <batrousseau@hotmail.com>
2026-05-29 19:48:24 +0200

    Initialization of task 6

---
+++ python-data-structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1 and not matrix[0]:
+       print()
+    else:
+        for i in range (len(matrix)):
+            for j in range (len(matrix)):
+                if (j < len(matrix) - 1):
+                    print("{:d}".format(matrix[i][j]), end=", ")
+                else:
+                    print("{:d}".format(matrix[i][j]))
