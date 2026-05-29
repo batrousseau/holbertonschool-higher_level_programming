463b0eb95cc268031ca537386e1ad2037d571f65
batrousseau <batrousseau@hotmail.com>
2026-05-29 20:20:31 +0200

    pycodestyle fix

--- python-data-structures/6-print_matrix_integer.py
+++ python-data-structures/6-print_matrix_integer.py
@@ -2,10 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1 and not matrix[0]:
-       print()
+        print()
     else:
-        for i in range (len(matrix)):
-            for j in range (len(matrix[0])):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if (j < len(matrix[0]) - 1):
                     print("{:d}".format(matrix[i][j]), end=" ")
                 else:
