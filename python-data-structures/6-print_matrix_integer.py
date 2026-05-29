c03c01a37a4b7207f2262688257aba411d4ecc65
batrousseau <batrousseau@hotmail.com>
2026-05-29 20:13:31 +0200

    New structure for non-squarred matrix

--- python-data-structures/6-print_matrix_integer.py
+++ python-data-structures/6-print_matrix_integer.py
@@ -5,8 +5,8 @@ def print_matrix_integer(matrix=[[]]):
        print()
     else:
         for i in range (len(matrix)):
-            for j in range (len(matrix)):
-                if (j < len(matrix) - 1):
+            for j in range (len(matrix[0])):
+                if (j < len(matrix[0]) - 1):
                     print("{:d}".format(matrix[i][j]), end=" ")
                 else:
                     print("{:d}".format(matrix[i][j]))
