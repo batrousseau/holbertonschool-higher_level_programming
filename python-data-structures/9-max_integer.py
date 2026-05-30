0c1d73a7975202a0e52eb9adb37ffdcf897cd010
batrousseau <batrousseau@hotmail.com>
2026-05-30 18:11:43 +0200

    Modifying max initialization at none

--- python-data-structures/9-max_integer.py
+++ python-data-structures/9-max_integer.py
@@ -3,7 +3,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return (None)
-    max = 0
+    max = None
     for i in range(len(my_list)):
         if my_list[i] > max:
             max = my_list[i]
