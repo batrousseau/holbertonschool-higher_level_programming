a401ce812c4849a09f4b2155222d051271b07c7f
batrousseau <batrousseau@hotmail.com>
2026-05-30 18:03:37 +0200

    Initialization of task 8

---
+++ python-data-structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if not sentence:
+        return (0, '')
+    else:
+        lenght: int = len(sentence)
+        first: str = sentence[:1]
+        return (lenght, first)
