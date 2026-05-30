2d03d1e1488dbbf673723b221081efd4b88a22ab
batrousseau <batrousseau@hotmail.com>
2026-05-30 17:23:12 +0200

    Pycodestyle change + dealing with empty tuple case

--- python-data-structures/7-add_tuple.py
+++ python-data-structures/7-add_tuple.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    #creating mutables int
-    a0: int = 0
-    a1: int = 0
-    b0: int = 0
-    b1: int = 0
-    #checking for NULL value in a then in B
-    if not tuple_a:
-        return(tuple_b)
-    if not tuple_b:
-        return(tuple_a)
-    #checking for minimum lenght of each tuple
-    if len(tuple_a) < 2:
-        a0 = tuple_a[0]
-    if len(tuple_a) == 2:
-        a0 = tuple_a[0]
-        a1 = tuple_a[1]
-    if len(tuple_b) < 2:
-        b0 = tuple_b[0]
-    if len(tuple_b) == 2:
-        b0 = tuple_b[0]
-        b1 = tuple_b[1]
-    tuple_c = ((a0 + b0), (a1 + b1))
-    return(tuple_c)
+    # checking for NULL value in a then in B
+    if not tuple_a and not tuple_b:
+        empty_tuple: tuple = ()
+        return (empty_tuple)
+    # checking for minimum lenght of each tuple
+    a_clean: tuple = (tuple_a + (0, 0))[:2]
+    b_clean: tuple = (tuple_b + (0, 0))[:2]
+    tuple_c: tuple = ((a_clean[0] + b_clean[0]), (a_clean[1]) + b_clean[1])
+    return (tuple_c)
