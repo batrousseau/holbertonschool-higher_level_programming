8c82fff182dcda2f4b3f65a83a3835fd3fc9cd25
batrousseau <batrousseau@hotmail.com>
2026-05-31 16:35:43 +0200

    Initialization of task 6

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if not a_dictionary:
+        return ("")
+    a_dictionary = sorted(a_dictionary.items())
+    sort_dir = dict(a_dictionary)
+    for i, j in sort_dir.items():
+        print(f"{i} : {j}")
