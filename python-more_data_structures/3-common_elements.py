d51f2f3fb506d34b54ef901f291195e6f291fd6c
batrousseau <batrousseau@hotmail.com>
2026-05-31 14:28:21 +0200

    Modifying empty set return

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
-    if not set_2 or not set_2:
-        return (None)
+    if not set_1 or not set_2:
+        return (set())
     set_1 = set_1.intersection(set_2)
     return (set_1)
