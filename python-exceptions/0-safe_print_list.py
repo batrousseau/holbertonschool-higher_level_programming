3878fef31dcfd57b87866ec3c85f787b196b5b28
batrousseau <batrousseau@hotmail.com>
2026-06-01 19:38:39 +0200

    Initialization of dir

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    number_printed: int = int()
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            number_printed = number_printed + 1
+        except ValueError:
+            i = + 1
+        except IndexError:
+            break
+    print()
+    return (number_printed)
