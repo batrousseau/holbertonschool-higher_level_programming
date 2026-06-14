95adccf24360d0d20bb053dd4a9dc5516b816158
batrousseau <batrousseau@hotmail.com>
2026-06-14 22:05:49 +0200

    Adding documentation to function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""
-matrix-divided : divide each value of the matrix
-by the number div
-matrix: matrix of integer or float
-div: divider for each value of the matrix
-Return: new matrix divided
-"""
 
+"""
+This module provides matrix manipulation utilities.
+
+It contains a single function, `matrix_divided`, which performs element-wise
+division on a 2D matrix (list of lists) containing integers or floats.
+The module validates input shapes, type consistency, and prevents division
+by zero errors by raising appropriate exceptions (TypeError, ZeroDivisionError).
+"""
 
 def matrix_divided(matrix, div):
+    """
+    matrix-divided : divide each value of the matrix
+    by the number div
+    matrix: matrix of integer or float
+    div: divider for each value of the matrix
+    Return: new matrix divided
+    """
     if not matrix and not matrix[0]:
         return (None)
     if not isinstance(div, int) and not isinstance(div, float):
