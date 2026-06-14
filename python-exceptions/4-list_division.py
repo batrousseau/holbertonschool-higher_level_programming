db8a658eb6d50fb41cfb5cc72c999b32e1da4210
batrousseau <batrousseau@hotmail.com>
2026-06-14 16:02:40 +0200

    Fixing a typo in type error message

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except TypeError:
             new_list.append(0)
-            print("Wrong type")
+            print("wrong type")
         finally:
             continue
 
