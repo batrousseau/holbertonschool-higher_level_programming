e0652f70ff475a5cbcb21bb4310c3cc39cbe5990
batrousseau <batrousseau@hotmail.com>
2026-06-14 18:16:20 +0200

    pycodestyle validation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """
 add_integer - function that sums two integers or float
 a: first integer or float
@@ -7,7 +8,8 @@ b: second integer or float, 98 by default
 Return: sum of a and b
 """
 
-def add_integer(a, b=98)->int :
+
+def add_integer(a, b=98) -> int:
     """
     Adds two integers.
 
@@ -20,18 +22,18 @@ def add_integer(a, b=98)->int :
     TypeError: a must be an integer
     """
 
-    if  a is None:
+    if a is None:
         raise TypeError("a must be an integer")
-    if  b is None: 
+    if b is None:
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
-    if isinstance (b, float):
+    if isinstance(b, float):
         b = int(b)
     if not isinstance(a, int):
         raise TypeError("a must be an integer")
     if not isinstance(b, int):
         raise TypeError("b must be an integer")
-    
+
     sum: int = a + b
-    return(sum)
+    return (sum)
