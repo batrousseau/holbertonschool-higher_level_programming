9d9e923d5d1f1c7e2891786d87ac660ac6058b89
batrousseau <batrousseau@hotmail.com>
2026-06-30 19:46:44 +0200

    Initialization of task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Square module.
+
+This module provides a simple definition of a Square class
+for basic geometric manipulations.
+"""
+
+
+class Square:
+    """Represents a square geometric shape."""
+
+    def __init__(self, size):
+        """Initialize a new instance of the Square class."""
+        self.__size = size
