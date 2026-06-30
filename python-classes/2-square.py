88898a6cc3cdc7e2db57e326ed0014e19a144d3d
batrousseau <batrousseau@hotmail.com>
2026-06-30 19:55:20 +0200

    Initialization of task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Square module.
+
+This module provides a simple definition of a Square class
+for basic geometric manipulations.
+"""
+
+
+class Square:
+    """Represents a square geometric shape."""
+
+    def __init__(self, size=0):
+        """Initialize a new instance of the Square class."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
