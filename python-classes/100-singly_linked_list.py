eae19dac542a8683b50153fd0c181252c6099c97
batrousseau <batrousseau@hotmail.com>
2026-07-08 18:39:52 +0200

    Modifying empty sll case with empty string

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -72,8 +72,8 @@ class SinglyLinkedList():
         Defines the string representation of the list.
         Returns all elements as a string, each separated by a newline character
         """
-        if self.__head == None:
-            return
+        if self.__head is None:
+            return ("")
         node_temp = self.__head
         to_print: str = ""
         while node_temp.next_node is not None:
