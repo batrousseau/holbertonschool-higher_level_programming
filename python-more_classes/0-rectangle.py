4309aa18261d2e04f3fec7fb7a7d2db4cafa9299
batrousseau <batrousseau@hotmail.com>
2026-07-13 19:02:11 +0200

    Initialization of repo with correct naming

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""
+Rectangle class definition.
+
+This Python script defines a simple rectangle class with basic
+properties and methods for geometric calculations.
+"""
+
+
+class Rectangle():
+    """
+    A basic representation of a rectangle in a 2D plane.
+    """
+
+    pass
