cc8399ddc96f0501a48da578663259075e128870
batrousseau <batrousseau@hotmail.com>
2026-07-14 16:47:32 +0200

    Initialization of task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+
+"""
+Rectangle class definition.
+
+This Python script defines a simple rectangle class with basic
+properties and methods for geometric calculations.
+"""
+
+
+class Rectangle():
+    """
+    A basic representation of a rectangle in a 2D plane.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self._height
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
