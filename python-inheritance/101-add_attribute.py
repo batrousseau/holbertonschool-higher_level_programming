f2fd1c3c2407e45a3d420abb8077d3d3076e4b2c
batrousseau <batrousseau@hotmail.com>
2026-07-24 17:45:55 +0200

    Initialization of task 101

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+"""Module providing a utility to add attributes dynamically.
+
+This module defines the add_attribute function which attempts
+to set a new attribute on an object if it is mutable and does not
+already possess that attribute."""
+
+
+def add_attribute(obj, name, value):
+    """Add a new attribute 'name' with given 'value' to 'obj'.
+
+    This function checks if 'obj' allows dynamic attributes.
+    It raises TypeError if the object type (str, int, tuple) forbids
+    it or if the attribute already exists."""
+
+    if hasattr(obj, name):
+        raise TypeError("can't add new attribute")
+    if type(obj) is str or type(obj) is int or type(obj) is tuple:
+        raise TypeError("can't add new attribute")
+    obj.name = value
