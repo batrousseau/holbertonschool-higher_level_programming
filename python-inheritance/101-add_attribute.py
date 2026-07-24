42b77d2cb44bc263f41951abf6b135395c686305
batrousseau <batrousseau@hotmail.com>
2026-07-24 18:04:49 +0200

    Going back to previous version to better understand missing checker with a non existing object

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -15,8 +15,6 @@ def add_attribute(obj, name, value):
     It raises TypeError if the object type (str, int, tuple) forbids
     it or if the attribute already exists."""
 
-    if obj not in globals():
-        raise TypeError("can't add new attribute")
     if hasattr(obj, name):
         raise TypeError("can't add new attribute")
     if type(obj) is str or type(obj) is int or type(obj) is tuple:
