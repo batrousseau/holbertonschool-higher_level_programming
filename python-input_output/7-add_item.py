366fa194f6b6cbae730f688cd34303bd382adfcd
batrousseau <batrousseau@hotmail.com>
2026-08-11 17:01:43 +0200

    Moving module comment at the top

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import sys
-from pathlib import Path
 
 """Module providing a utility to manage JSON item lists.
 
@@ -8,6 +6,8 @@ This script checks if 'add_item.json' exists, loads existing data
 or initializes empty list, appends command line argument and
 saves updated content back."""
 
+import sys
+from pathlib import Path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
