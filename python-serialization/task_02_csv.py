50aceafc01063236f5a89d8d585e93aa10699140
batrousseau <batrousseau@hotmail.com>
2026-08-13 10:57:53 +0200

    Initialization of task 2

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import csv
+import json
+
+def convert_csv_to_json(csv_file):
+    try:
+        with open(csv_file, mode="r",newline='',encoding="utf-8") as file:
+            data = list(csv.DictReader(file))
+    except FileNotFoundError:
+            print(f"Erreur : Le fichier '{file}' est introuvable.")
+    except (PermissionError, OSError) as e:
+         print(f"Erreur d'accès au fichier : {e}")
+
+    try:
+        with open("data.json", mode="w", encoding="utf-8") as json_file:
+            json.dump(data,json_file, indent=4)
+    except (EOFError, AttributeError, ImportError) as e:
+            print(f"Erreur de désérialisation : fichier corrompu ou incomplet ({e})")
