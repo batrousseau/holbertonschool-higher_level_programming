fcf1f5bcf542768c893f17b18899f6c5a4a5fa6e
batrousseau <batrousseau@hotmail.com>
2026-08-13 11:16:52 +0200

    Implementing return

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,12 +8,15 @@ def convert_csv_to_json(csv_file):
         with open(csv_file, mode="r",newline='',encoding="utf-8") as file:
             data = list(csv.DictReader(file))
     except FileNotFoundError:
-            print(f"Erreur : Le fichier '{file}' est introuvable.")
+        print(f"Erreur : Le fichier '{file}' est introuvable.")
+        return False
     except (PermissionError, OSError) as e:
-         print(f"Erreur d'accès au fichier : {e}")
+        print(f"Erreur d'accès au fichier : {e}")
+        return False
 
     try:
         with open("data.json", mode="w", encoding="utf-8") as json_file:
             json.dump(data,json_file, indent=4)
+            return True
     except (EOFError, AttributeError, ImportError) as e:
             print(f"Erreur de désérialisation : fichier corrompu ou incomplet ({e})")
